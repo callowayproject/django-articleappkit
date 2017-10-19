d301f2d511a65461eedbcc301955dafecba189ca
Corey Oordt <coreyoordt@gmail.com>
2017-10-19 13:10:40 -0500

    Ignoring build dir

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 import os
+import sys
 from setuptools import setup, find_packages
+import articleappkit
+
+version = articleappkit.get_version()
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py bdist_wheel upload')
+    print("You probably want to also tag the version now:")
+    print("  python setup.py tag")
+    sys.exit()
+elif sys.argv[-1] == 'tag':
+    cmd = "git tag -a %s -m 'version %s';git push --tags" % (version, version)
+    os.system(cmd)
+    sys.exit()
 
 
 def read_file(filename):
@@ -20,17 +34,17 @@ def get_readme():
     return ''
 
 # Use the docstring of the __init__ file to be the description
-DESC = " ".join(__import__('articleappkit').__doc__.splitlines()).strip()
+DESC = " ".join(articleappkit.__doc__.splitlines()).strip()
 
 setup(
     name="django-articleappkit",
-    version=__import__('articleappkit').get_version().replace(' ', '-'),
+    version=version.replace(' ', '-'),
     url='http://callowayproject.com/',
     author='Calloway Project',
     author_email='webmaster@callowayproject.com',
     description=DESC,
     long_description=get_readme(),
-    packages=find_packages(),
+    packages=find_packages(exclude=('example', 'example*', )),
     include_package_data=True,
     install_requires=read_file('requirements.txt'),
     classifiers=[
